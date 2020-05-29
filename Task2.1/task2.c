eb199b2f423483b3c810c30037057de540f47603
incomprehensible <inc0mprehensible@protonmail.com>
2020-05-29 07:25:15 +0300

    extra

---
+++ Task2.1/task2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void bubble_sort(int *arr, int sz)
+{
+	int tmp;
+
+	for (int i = 0; i < sz; ++i)
+	{
+		for (int j = 0; j<sz-i-1; ++j)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				tmp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = tmp;
+			}
+		}
+	}
+	printf("After sorting: ");
+	for (int i = 0; i < sz; ++i)
+		printf("%d ", arr[i]);
+	putchar('\n');
+}
+
+int main()
+{
+	int arr[] = { 1, 90, -500, 0, 0, 1, 234, 12345, 700, -50, -5, -100};
+	int sz = sizeof(arr) / sizeof(int);
+	
+	printf("Before routine: ");
+	for (int i = 0; i < sz; ++i)
+		printf("%d ", arr[i]);
+
+	putchar('\n');
+	
+	bubble_sort(&arr[0], sz);
+
+	return 0;
+}
